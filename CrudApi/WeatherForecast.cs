3e2ad7d43a147b97420b884c22dbe97f462955fa
Irfan <irfan.atjava@gmail.com>
2019-12-17 14:19:45 +0530

    added dummy change - modified release pipeline to select CrudApi.zip

--- CrudApi/WeatherForecast.cs
+++ CrudApi/WeatherForecast.cs
@@ -1,14 +1,12 @@
 using System;
 
-namespace CrudApi
-{
-    public class WeatherForecast
-    {
+namespace CrudApi {
+    public class WeatherForecast {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        // some dummy change
+        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 
         public string Summary { get; set; }
     }
